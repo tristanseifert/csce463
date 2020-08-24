c56907cb9215361a65b6b7a55a93676f4c6168a8
Tristan Seifert <tristan@tseifert.me>
2020-08-24 12:27:44 -0500

    Initial web client solution

---
+++ WebClient/WebClient/pch.h
@@ -0,0 +1,13 @@
+/**
+ * Precompiled header file; this contains all the includes
+ */
+#pragma once
+
+// Standard C libs
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+
+// HTML parser
+#include "HTMLParserBase.h"
